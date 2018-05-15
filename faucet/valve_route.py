e0d899750db8146d6186627e6a6c5326a75b23ef
Bill Fisher <william.w.fisher@gmail.com>
2018-05-15 15:40:45 -0700

    Porting fix.

--- faucet/valve_route.py
+++ faucet/valve_route.py
@@ -886,7 +886,7 @@ class ValveIPv6RouteManager(ValveRouteManager):
         icmpv6_pkt = pkt_meta.pkt.get_protocol(icmpv6.icmpv6)
         if icmpv6_pkt is None:
             return ofmsgs
-        icmpv6_type = icmpv6_pkt.type_
+        icmpv6_type = icmpv6_pkt.icmpv6_type
         if (ipv6_pkt.hop_limit != valve_packet.IPV6_MAX_HOP_LIM and
                 icmpv6_type != icmpv6.ICMPV6_ECHO_REQUEST):
             return ofmsgs
